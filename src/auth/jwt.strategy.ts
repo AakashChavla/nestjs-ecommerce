44f099faff65149eb0eb92701f842ea6a0d27ac1
Aakash Chavla <akash.cilans@gmail.com>
2025-05-23 17:46:32 +0530

    new project started

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from "../users/users.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(private usersService: UsersService){}
+    super({
+        jwtFromRequest: ExtractJwt.fromAuthHearder
+    })
+}
